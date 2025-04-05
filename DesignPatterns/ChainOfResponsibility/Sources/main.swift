3eed4af6a04e25717f80fc9ac8b3afdac5300adf
Robert Walker <robertwalker1@icloud.com>
2025-04-05 15:37:02 -0400

    Add Chain of Responsponsibility demo

---
+++ DesignPatterns/ChainOfResponsibility/Sources/main.swift
@@ -0,0 +1,35 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+print("Strategy Pattern Demo")
+print("=======================")
+
+class MainApplication {
+    func run() {
+        // Setup the Chain of Responsibility
+        let mainHandler = ActivationHandlerNew()
+        let handler2 = ActivationHandlerMigrate()
+        let handler3 = ActivationHandlerPort()
+
+        mainHandler.next = handler2
+        handler2.next = handler3
+
+        for account in accounts {
+            mainHandler.activate(account: account)
+        }
+        print("=======================")
+    }
+}
+
+// Create some test data to emulate user input or message queue events
+let accounts = [
+    Account(id: 1, action: .create, balance: 20_000.00),
+    Account(id: 2, action: .migrate, balance: 15_000.00),
+    Account(id: 3, action: .port, balance: 5_000.00),
+    Account(id: 4, action: .migrate, balance: 25_000.00),
+    Account(id: 5, action: .create, balance: 30_000.00),
+    Account(id: 6, action: .unknown, balance: 0.00),
+]
+
+let app = MainApplication()
+app.run()
---
+++ demos/ChainOfResponsibilityPattern.md
@@ -0,0 +1,26 @@
+# Chain of Responsibility Pattern
+
+The "Chain of Responsibility" is a behavioral design pattern that move the decision logic into isolated "handler"
+objects. A more thorough explanation can be found here []().
+
+Here is a diagram of a very basic Chain of Responsibility pattern:
+
+```mermaid
+---
+config:
+  look: handDrawn
+  theme: neutral
+---
+graph TB
+    A[Request] --> B(Handler)
+    B -- next --> C[Handler]
+    B -- handled --> E[End]
+    C -- next --> D[Handler]
+    C -- handed --> E[End]
+    D --> E[End]
+```
+
+## Swift Demo Project
+
+Here is the [README](../DesignPatterns/ChainOfResponsibility/README.md) for the demo project. To run the demo open the
+[Package.swift](../DesignPatterns/ChainOfResponsibility/Package.swift) file in Xcode and press the play button.
